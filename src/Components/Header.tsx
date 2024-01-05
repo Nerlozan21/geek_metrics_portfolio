efab11fe060ad00e970011abdbcc23801878b17c
Nerlozan <59479702+Nerlozan21@users.noreply.github.com>
2024-01-06 00:36:11 +0530

    Updated for about Us page

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,21 +5,10 @@ const Header = () => {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light py-3">
         <div className="container">
-          <a href="#" className="navbar-brand d-flex align-items-center">
+          <a href="/" className="navbar-brand d-flex align-items-center">
             <i className="fa fa-snowflake-o fa-lg text-primary mr-2"></i>
             <strong>GeekMetrics</strong>
           </a>
-          <button
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            className="navbar-toggler"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div id="navbarSupportedContent" className="collapse navbar-collapse">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
@@ -29,7 +18,7 @@ const Header = () => {
                 </a>
               </li>
               <li className="nav-item active">
-                <a href="" className="nav-link font-italic">
+                <a href="../aboutUs" className="nav-link font-italic">
                   {" "}
                   About{" "}
                 </a>
