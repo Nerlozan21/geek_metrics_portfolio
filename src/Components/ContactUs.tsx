1503e61417f4a5e4f7579c85ab5f633f17e41d6a
Nerlozan <59479702+Nerlozan21@users.noreply.github.com>
2024-01-08 22:24:26 +0530

    update for contact us

--- src/Components/ContactUs.tsx
+++ src/Components/ContactUs.tsx
@@ -3,105 +3,143 @@ import "../Styles/ContactUsStyles.css";
 const ContactUs = () => {
   return (
     <div>
-      <section className="contact_us">
+      <section>
+        <div className="section-header">
+          <div className="container">
+            <h2>
+              Contact <span style={{ color: "#5535f0" }}> Us</span>
+            </h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+
         <div className="container">
           <div className="row">
-            <div className="col-md-10 offset-md-1">
-              <div className="contact_inner">
-                <div className="row">
-                  <div className="col-md-10">
-                    <div className="contact_form_inner">
-                      <div className="contact_field">
-                        <h3>Contact Us</h3>
-                        <p>
-                          Feel Free to contact us any time. We will get back to
-                          you as soon as we can!.
-                        </p>
-                        <input
-                          type="text"
-                          className="form-control form-group"
-                          placeholder="Name"
-                        />
-                        <input
-                          type="text"
-                          className="form-control form-group"
-                          placeholder="Email"
-                        />
-                        <textarea
-                          className="form-control form-group"
-                          placeholder="Message"
-                        ></textarea>
-                        <button className="contact_form_submit">Send</button>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div className="right_conatct_social_icon d-flex align-items-end">
-                      <div className="socil_item_inner d-flex">
-                        <li>
-                          <a href="#">
-                            <i className="fab fa-facebook-square"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="fab fa-instagram"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="fab fa-twitter"></i>
-                          </a>
-                        </li>
-                      </div>
-                    </div>
-                  </div>
+            <div className="contact-info text">
+              <div className="contact-info-item">
+                <div className="contact-info-icon p-3 bg-transparent">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    fill="currentColor"
+                    className="bi bi-pin-map-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z"
+                    />
+                  </svg>
                 </div>
-                <div className="contact_info_sec">
-                  <h4>Contact Info</h4>
-                  <div className="d-flex info_single align-items-center">
-                    <i className="fas fa-headset"></i>
-                    <span>+91 8009 054294</span>
-                  </div>
-                  <div className="d-flex info_single align-items-center">
-                    <i className="fas fa-envelope-open-text"></i>
-                    <span>infoflightmantra.com</span>
-                  </div>
-                  <div className="d-flex info_single align-items-center">
-                    <i className="fas fa-map-marked-alt"></i>
-                    <span>
-                      1000+ Travel partners and 65+ Service city across India,
-                      USA, Canada & UAE
-                    </span>
-                  </div>
+
+                <div className="contact-info-content">
+                  <h4>Address</h4>
+                  <p>
+                    Jbr Tech Park, 6th Rd, Whitefield, EPIP Zone,
+                    <br />
+                    Whitefield, Bengaluru, Karnataka <br />
+                    560066
+                  </p>
                 </div>
               </div>
+
+              <div className="contact-info-item">
+                <div className="contact-info-icon p-3 bg-transparent">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    fill="currentColor"
+                    className="bi bi-telephone-outbound-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5"
+                    />
+                  </svg>
+                </div>
+
+                <div className="contact-info-content">
+                  <h4>Phone</h4>
+                  <p>+91 9035521136</p>
+                </div>
+              </div>
+
+              <div className="contact-info-item">
+                <div className="contact-info-icon p-3 bg-transparent">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    fill="currentColor"
+                    className="bi bi-envelope-at-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671" />
+                    <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791Z" />
+                  </svg>
+                </div>
+
+                <div className="contact-info-content">
+                  <h4>Email</h4>
+                  <p>info&#64;geeksmetrics.in</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="contact-form bg-transparent">
+              <form action="" id="contact-form">
+                <h2 className="text-white">Send Message</h2>
+                <div className="input-box">
+                  <input type="text" required name="" />
+                  <span>Full Name</span>
+                </div>
+
+                <div className="input-box">
+                  <input type="email" required name="" />
+                  <span>Email</span>
+                </div>
+
+                <div className="input-box">
+                  <textarea required name=""></textarea>
+                  <span>Type your Message...</span>
+                </div>
+
+                <div className="input-box">
+                  <input type="submit" value="Send" name="" />
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="map_sec">
-        <div className="container">
+      <section className="map_sec py-5">
+        <div className="container mb-5">
           <div className="row">
             <div className="col-md-10 offset-md-1">
               <div className="map_inner">
-                <h4>Find Us on Google Map</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Tempore quo beatae quasi assumenda, expedita aliquam minima
-                  tenetur maiores neque incidunt repellat aut voluptas hic
-                  dolorem sequi ab porro, quia error.
-                </p>
+                <h4 className="text-white">Find Us on Google Map</h4>
+
                 <div className="map_bind">
-                  {/* <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62209.82203830085!2d77.69533287369288!3d12.964564261476536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae11f35d0dfc83%3A0x30cfa512d80115f9!2sWhitefield%2C%20Bengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1702557988093!5m2!1sen!2sin"
-              width="100%"
-              height="450"
-              allowfullscreen=""
-              aria-hidden="false"
-              tabindex="0"
-            ></iframe> */}
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.8985174585014!2d77.72499707590217!3d12.978342214728753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae11fbbbc36817%3A0x1441e10c2bb64441!2sBHIVE%20Workspace%20Whitefield%20Campus!5e0!3m2!1sen!2sin!4v1703009564619!5m2!1sen!2sin"
+                    width="100%"
+                    height="450"
+                    style={{ border: 0 }}
+                    aria-hidden="false"
+                  ></iframe>
                 </div>
               </div>
             </div>
