e5e3d18cbe948dcf749ff7108fa816b4f7dc07fe
Nerlozan <59479702+Nerlozan21@users.noreply.github.com>
2024-01-08 21:33:29 +0530

    Update for About Us page

--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -3,18 +3,20 @@ import "../Styles/AboutUsStyles.css";
 const AboutUs = () => {
   return (
     <div>
-      <div className="bg-light">
+      <div className="text-white">
         <div className="container py-5">
           <div className="row h-100 align-items-center py-5">
             <div className="col-lg-6">
-              <h1 className="display-4">About us page</h1>
-              <p className="lead text-muted mb-0">
+              <h1 className="display-4 text-white">
+                GeeksMetrics Solutions Private Ltd.
+              </h1>
+              <p className="lead mb-0">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Recusandae fugit molestiae, mollitia minus consequuntur
                 asperiores. Numquam repellendus amet impedit nulla. Perferendis
                 laboriosam optio eveniet corporis?
               </p>
-              <p className="lead text-muted"></p>
+              <p className="lead"></p>
             </div>
             <div className="col-lg-6 d-none d-lg-block">
               <img
@@ -27,19 +29,22 @@ const AboutUs = () => {
         </div>
       </div>
 
-      <div className="bg-white py-5">
+      <div className="py-5">
         <div className="container py-5">
           <div className="row align-items-center mb-5">
             <div className="col-lg-6 order-2 order-lg-1">
               <i className="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-              <h2 className="font-weight-light">Lorem ipsum dolor sit amet</h2>
-              <p className="font-italic text-muted mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <h2 className="font-weight-light text-white">Mission</h2>
+              <p className="font-italic mb-4">
+                GeeksMetrics care of every aspect of endpoint and user security
+                in the most detailed way, as well as easily implements it. The
+                dependence between user, device, and corporate data is
+                systematically aligned to provide the best security and control
+                needed to manage enterprise endpoints under one console. With a
+                technology that is future-proof and edging towards complete
+                automation and hands-free management, GeeksMetrics continues to
+                make businesses secure.
               </p>
-              <a href="#" className="btn btn-light px-5 rounded-pill shadow-sm">
-                Learn More
-              </a>
             </div>
             <div className="col-lg-5 px-5 mx-auto order-1 order-lg-2">
               <img
@@ -59,25 +64,31 @@ const AboutUs = () => {
             </div>
             <div className="col-lg-6">
               <i className="fa fa-leaf fa-2x mb-3 text-primary"></i>
-              <h2 className="font-weight-light">Lorem ipsum dolor sit amet</h2>
-              <p className="font-italic text-muted mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <h2 className="font-weight-light text-white">Vision</h2>
+              <p className="mb-4">
+                Businesses always adopt new technologies to enhance work and
+                productivity. When technologies change at a rapid pace,
+                businesses require a solution that adapts to these frequent
+                changes for managing the corporate endpoints and data. Being a
+                security management solution, GeeksMetrics understands the risks
+                and requirements of enterprises to utilize an encompassing
+                platform that covers all security bases. GeeksMetrics helps
+                close the gap between the mobile endpoints and IT, plus makes
+                security and management of endpoints simple.
               </p>
-              <a href="#" className="btn btn-light px-5 rounded-pill shadow-sm">
-                Learn More
-              </a>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="bg-light py-5">
+      <div className="py-5">
         <div className="container py-5">
           <div className="row mb-4">
             <div className="col-lg-5">
-              <h2 className="display-4 font-weight-light">Our team</h2>
-              <p className="font-italic text-muted">
+              <h2 className="display-4 font-weight-light text-white">
+                Our <span style={{ color: "#5535f0" }}>team</span>
+              </h2>
+              <p className="font-italic">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               </p>
             </div>
@@ -85,36 +96,36 @@ const AboutUs = () => {
 
           <div className="row text-center">
             <div className="col-xl-3 col-sm-6 mb-5">
-              <div className="bg-white rounded shadow-sm py-5 px-4">
+              <div className="rounded shadow-sm py-5 px-4 bg-white">
                 <img
                   src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png"
                   alt=""
                   width="100"
                   className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
                 />
-                <h5 className="mb-0">Syed Janath Hussen</h5>
-                <span className="small text-uppercase text-muted">
+                <h5 className="mb-0 bg-white">Syed Janath Hussen</h5>
+                <span className="small text-uppercase bg-white text-black">
                   CEO - Founder
                 </span>
-                <ul className="social mb-0 list-inline mt-3">
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-facebook-f"></i>
+                <ul className="social mb-0 list-inline mt-3 bg-white">
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-facebook-f text-black bg-white"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-twitter"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-twitter text-black bg-white"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-instagram"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-instagram bg-white text-black"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-linkedin"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-linkedin bg-white text-black"></i>
                     </a>
                   </li>
                 </ul>
@@ -122,36 +133,36 @@ const AboutUs = () => {
             </div>
 
             <div className="col-xl-3 col-sm-6 mb-5">
-              <div className="bg-white rounded shadow-sm py-5 px-4">
+              <div className="rounded shadow-sm py-5 px-4 bg-white">
                 <img
-                  src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png"
+                  src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png"
                   alt=""
                   width="100"
                   className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
                 />
-                <h5 className="mb-0">Syed Janath Hussen</h5>
-                <span className="small text-uppercase text-muted">
+                <h5 className="mb-0 bg-white">Syed Janath Hussen</h5>
+                <span className="small text-uppercase bg-white text-black">
                   CEO - Founder
                 </span>
-                <ul className="social mb-0 list-inline mt-3">
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-facebook-f"></i>
+                <ul className="social mb-0 list-inline mt-3 bg-white">
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-facebook-f text-black bg-white"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-twitter"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-twitter text-black bg-white"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-instagram"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-instagram bg-white text-black"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-linkedin"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-linkedin bg-white text-black"></i>
                     </a>
                   </li>
                 </ul>
@@ -159,36 +170,36 @@ const AboutUs = () => {
             </div>
 
             <div className="col-xl-3 col-sm-6 mb-5">
-              <div className="bg-white rounded shadow-sm py-5 px-4">
+              <div className="rounded shadow-sm py-5 px-4 bg-white">
                 <img
-                  src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png"
+                  src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png"
                   alt=""
                   width="100"
                   className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
                 />
-                <h5 className="mb-0">Samuel Hardy</h5>
-                <span className="small text-uppercase text-muted">
+                <h5 className="mb-0 bg-white">Syed Janath Hussen</h5>
+                <span className="small text-uppercase bg-white text-black">
                   CEO - Founder
                 </span>
-                <ul className="social mb-0 list-inline mt-3">
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-facebook-f"></i>
+                <ul className="social mb-0 list-inline mt-3 bg-white">
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-facebook-f text-black bg-white"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-twitter"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-twitter text-black bg-white"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-instagram"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-instagram bg-white text-black"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-linkedin"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-linkedin bg-white text-black"></i>
                     </a>
                   </li>
                 </ul>
@@ -196,36 +207,36 @@ const AboutUs = () => {
             </div>
 
             <div className="col-xl-3 col-sm-6 mb-5">
-              <div className="bg-white rounded shadow-sm py-5 px-4">
+              <div className="rounded shadow-sm py-5 px-4 bg-white">
                 <img
-                  src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png"
+                  src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png"
                   alt=""
                   width="100"
                   className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
                 />
-                <h5 className="mb-0">Syed Janath Hussen</h5>
-                <span className="small text-uppercase text-muted">
+                <h5 className="mb-0 bg-white">Syed Janath Hussen</h5>
+                <span className="small text-uppercase bg-white text-black">
                   CEO - Founder
                 </span>
-                <ul className="social mb-0 list-inline mt-3">
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-facebook-f"></i>
+                <ul className="social mb-0 list-inline mt-3 bg-white">
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-facebook-f text-black bg-white"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-twitter"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-twitter text-black bg-white"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-instagram"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-instagram bg-white text-black"></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-linkedin"></i>
+                  <li className="list-inline-item bg-white">
+                    <a href="#" className="social-link bg-white">
+                      <i className="fa fa-linkedin bg-white text-black"></i>
                     </a>
                   </li>
                 </ul>
@@ -233,6 +244,104 @@ const AboutUs = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="py-2">
+        <div className="container py-2">
+          <div className="row mb-4">
+            <div className="col-lg-5">
+              <h2 className="display-4 font-weight-light text-white">
+                Projects
+              </h2>
+              <p className="font-italic">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+
+            <div className="row text-center justify-content-center">
+              <div
+                className="animate__animated animate bounce card m-4"
+                style={{ width: "18rem" }}
+              >
+                <div className="container mt-3">
+                  <img
+                    src="https://i.ibb.co/gRpP2Lm/icons8-online-128.png"
+                    className="card-img-top bg-white"
+                    alt="..."
+                  />
+                </div>
+                <div className="card-body bg-white">
+                  <h5 className="card-title ms-1 bg-white">
+                    Food Ordering Website
+                  </h5>
+                  <p className="card-text mb-5 ms-1 bg-white">
+                    See more bootstrap code snippets on my website
+                  </p>
+
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="btn btn-primary mb-1 mt-1"
+                  >
+                    Go now
+                  </a>
+                </div>
+              </div>
+              <div
+                className="animate__animated animate bounce card m-4"
+                style={{ width: "18rem" }}
+              >
+                <div className="container mt-3">
+                  <img
+                    src="https://i.ibb.co/gRpP2Lm/icons8-online-128.png"
+                    className="card-img-top bg-white"
+                    alt="..."
+                  />
+                </div>
+                <div className="card-body bg-white">
+                  <h5 className="card-title ms-1 bg-white">Shopping Website</h5>
+                  <p className="card-text mb-5 ms-1 bg-white">
+                    See more bootstrap code snippets on my website
+                  </p>
+
+                  <a
+                    href="https://freesnippetcode.blogspot.com/"
+                    target="_blank"
+                    className="btn btn-primary mb-1 mt-1"
+                  >
+                    Go now
+                  </a>
+                </div>
+              </div>
+              <div
+                className="animate__animated animate bounce card m-4"
+                style={{ width: "18rem" }}
+              >
+                <div className="container mt-3">
+                  <img
+                    src="https://i.ibb.co/gRpP2Lm/icons8-online-128.png"
+                    className="card-img-top bg-white"
+                    alt="..."
+                  />
+                </div>
+                <div className="card-body bg-white">
+                  <h5 className="card-title ms-1 bg-white">Portfolio</h5>
+                  <p className="card-text mb-5 ms-1 bg-white">
+                    Simple Protfolio website for customer
+                  </p>
+
+                  <a
+                    href="https://prathameshpendal.web.app/"
+                    target="_blank"
+                    className="btn btn-primary mb-1 mt-1"
+                  >
+                    Go now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
       </div>
     </div>
   );
